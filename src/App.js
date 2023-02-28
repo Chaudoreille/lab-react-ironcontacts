534653b2df7179fee8707ffcef0e7af2d12f9ad7
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-28 17:06:07 +0100

    [IT 4] - sort

--- src/App.js
+++ src/App.js
@@ -14,14 +14,27 @@ function App() {
             return [...contactList, filteredCelebs[randomNum]];
         });
     };
-    const sortByPopularity
+    const sortByName = () => {
+        setContacts((contactList) => {
+            contactList.sort((a, b) =>
+                a.name === b.name ? 0 : a.name < b.name ? -1 : 1
+            );
+            return [...contactList];
+        });
+    };
+    const sortByPopularity = () => {
+        setContacts((contactList) => {
+            contactList.sort((a, b) => a.popularity - b.popularity);
+            return [...contactList];
+        });
+    };
     return (
         <div className="App">
             <h1>IronContacts</h1>
             <div className="control-bar">
                 <button onClick={addRandomContact}>Add Random Contact</button>
-                <button onClick={SortByName}>Sort by Name</button>
-                <button onClick={SortByPopularity}>Sort by Popularity</button>
+                <button onClick={sortByName}>Sort by Name</button>
+                <button onClick={sortByPopularity}>Sort by Popularity</button>
             </div>
             <table className="ContactList">
                 <thead>
