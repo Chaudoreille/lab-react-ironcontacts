533622eb68b2d070f5a4342bc5ed36c262ae929b
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-28 15:13:17 +0100

    [IT 1] - Basic Page format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactDB from "./contacts.json";
+
+function ContactList(props) {
+    const { contacts, setContacts } = { ...props };
+    return (
+        <table className="ContactList">
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => {
+                    return (
+                        <tr>
+                            <td>
+                                <img src={contact.pictureUrl} alt="" />
+                            </td>
+                            <td>{contact.name}</td>
+                            <td>{Number(contact.popularity).toFixed(2)}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [contacts, setContacts] = useState(contactDB.slice(0, 5));
+    return (
+        <div className="App">
+            <h1>IronContacts</h1>
+            <ContactList contacts={contacts} setContacts={setContacts} />
+        </div>
+    );
 }
 
 export default App;
